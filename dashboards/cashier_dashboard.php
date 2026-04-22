ed79039bdc348f4e745b99678d3623bfaed7c30b
Quevenn28 <quevennendico28@gmail.com>
2026-04-22 12:54:37 +0800

    update

--- dashboards/cashier_dashboard.php
+++ dashboards/cashier_dashboard.php
@@ -12,76 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Cashier Dashboard</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="cashier_dashboard.css">
-
-
-=======
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-<style>
-
-    body {
-        font-family: Arial;
-        background: #f4f6f9;
-        margin: 0;
-    }
-
-    .container {
-        max-width: 500px;
-        margin: 50px auto;
-        background: white;
-        padding: 20px;
-        border-radius: 10px;
-    }
-
-    input, select, button {
-        width: 100%;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-
-    button {
-        background: #D53E0F;
-        color: white;
-        border: none;
-        cursor: pointer;
-    }
-
-    .logout_cashier{
-        position:absolute;
-        top:20px;
-        right:40px;
-    }
-
-    .logout{
-        font-size: 18px;
-    }
-    
-    /* .logout_cashier img{
-        width:30px;
-        position:absolute;
-    } */
-
-    .logout i{
-        padding-right:5px;
-    }
-
-    a{
-        text-decoration: none;
-        color:white;
-    }
-
-    .logout:hover{
-        background:darkred;
-    }
-
-    .weight{
-        width:95%;
-    }
-
-</style>
->>>>>>> 3b40e406f2ced1c35734451982e958a962b5ff77
 
 </head>
 
